60e4e0ef1c5d1b6e320e1597c6a71904a4b1530a
what is the dog doing <ksmith177@mail.pima.edu>
2024-10-17 23:34:16 -0700

    exercise 3.7
    i lowkey didnt realize that 2.8 didnt need a for loop and so i was like "ok i guess ill use a for loop for this since its easier" and turns out thats how the question wanted me to do it in the first place so thats cool

---
+++ 129/cis129_exc37.py
@@ -0,0 +1,7 @@
+# i never actually _did_ this exercise (2.8)
+# so i guess i'll just churn it out now and 
+# do it :Q_
+
+print("number\tsquare\tcube");
+for number in range(0, 6):
+    print(f"{number:>6}\t{number**2:>6}{number**3:>6}")
